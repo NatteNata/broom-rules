35e323d6c5b7e02e435af9f6a53451b0f377c5c6
NatteNata <natte@yandex.ru>
2025-05-03 00:14:30 +0300

    feat: error boundaries set-up

---
+++ src/app/global-error.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Wrapper } from '@components'
+import { useRouter } from 'next/navigation'
+import { Button, Typography } from 'penguin-ui'
+import { startTransition } from 'react'
+
+export default function ErrorBoundary({
+	error,
+	reset,
+}: {
+	error: Error & { digest?: string }
+	reset: () => void
+}) {
+	console.log('ErrorBoundary', error, error.message)
+	const router = useRouter()
+
+	const reload = () => {
+		startTransition(() => {
+			router.refresh()
+			reset()
+		})
+	}
+
+	return (
+		<html lang='en'>
+			<body>
+				<Wrapper>
+					<Typography
+						as={'h2'}
+						variant={'h3'}
+						className={'my-12 text-start font-bold text-danger-500'}
+					>
+						`Something unexpected went wrong. Click button below to attempt
+						recovery. /n ${error.message}`
+					</Typography>
+					<Button variant={'primary'} onClick={reload}>
+						Reset
+					</Button>
+				</Wrapper>
+			</body>
+		</html>
+	)
+}
